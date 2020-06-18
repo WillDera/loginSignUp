c7fbb03556d7d6c02fc11f61b19d7c6868af2db3
Godswill <30760648+WillDera@users.noreply.github.com>
2020-06-18 21:52:19 +0100

    Working

--- loginSignUp/urls.py
+++ loginSignUp/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('accounts.url')),
+    path('', include('accounts.urls')),
     path('admin/', admin.site.urls),
 ]
