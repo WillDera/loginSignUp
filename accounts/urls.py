d7f0e690b9d0b4e9ed10604f0dc4dceda4e709d4
Godswill <30760648+WillDera@users.noreply.github.com>
2020-06-19 19:58:36 +0100

    Editing and Fixing

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-    path('', views.index, name="index")
+    path('login/', views.login, name="login"),
+    path('signup/', views.signup, name="signup")
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -3,5 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 
 
-def index(request):
+def login(request):
     return render(request, 'accounts/login.html')
+
+
+def signup(request):
+    return render(request, 'accounts/signup.html')
